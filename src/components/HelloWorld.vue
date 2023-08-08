34218e1f69af62477f17dee785b1502473ae81ca
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2023-08-08 13:12:33 +0700

    Init code

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h3>Product</h3>
+    <ProducItem
+      v-for="product in products"
+      :key="product.id"
+      :id="product.id"
+      :name="product.name"
+      :image="product.image"
+      :price="product.price"
+      @add-to-cart="addToCart"
+    />
+  </div>
+</template>
+
+<script>
+import ProducItem from "./test/Product.vue";
+export default {
+  name: "HelloWorld",
+  components: { ProducItem },
+  props: {},
+  data() {
+    return {
+      products: [
+        { id: 1, name: "Product A", image: "https://images.unsplash.com/photo-1661956601031-4cf09efadfce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2076&q=80", price: 19.99 },
+        { id: 2, name: "Product B", image: "https://images.unsplash.com/photo-1637684666451-423047d6bf5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80", price: 29.99 },
+      ],
+      cart: [],
+    };
+  },
+  methods: {
+    addToCart(productId) {
+      this.cart.push(productId);
+    },
+    created() {
+      this.isLog();
+    },
+    mounted() {},
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.product-list {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/test/Product.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="product">
+    <img :src="image" :alt="name" class="product-image" />
+    <h3 class="product-name">{{ name }}</h3>
+    <p class="product-price">{{ formatPrice(price) }}</p>
+    <button @click="addToCart" class="add-to-cart-button">Add to Cart</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProducItem",
+  props: {
+    id: Number,
+    name: String,
+    image: String,
+    price: Number,
+  },
+  data() {
+    return {
+      log: "",
+    };
+  },
+  mounted() {
+    this.onLog();
+    console.log('Component Mouted');
+  },
+  methods: {
+    onLog() {
+      console.log("On Log");
+    },
+
+    addToCart() {
+      this.$emit("add-to-cart", this.id);
+      this.onLog();
+    },
+
+    formatPrice(price) {
+      return `$${price.toFixed(2)}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.product {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 10px;
+  text-align: center;
+}
+
+.product-image {
+  max-width: 500px;
+  height: auto;
+}
+
+.product-name {
+  font-size: 1.2rem;
+  margin-top: 5px;
+}
+
+.product-price {
+  font-weight: bold;
+  color: #007bff;
+}
+
+.add-to-cart-button {
+  margin-top: 10px;
+  padding: 5px 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
